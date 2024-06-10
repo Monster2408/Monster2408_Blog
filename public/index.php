b17256587ef7d26a7505ed8996cf1548c26fb1de
Monster2408 <meoto2408@gmail.com>
2024-06-11 01:42:24 +0900

    up

--- public/index.php
+++ public/index.php
@@ -5,6 +5,15 @@ $func = new MyFunction('./assets/config.php');
 
 $func->addStyle('/assets/css/style.min.css');
 
+function is_page() {
+    return empty($_GET['p']) === false;
+}
+
+$top_image = $func->getUrl() . '/assets/img/monster2408-blog-top.jpg';
+if (is_page()) {
+    $top_image = $func->getUrl() . '/assets/img/monster2408-blog-top.jpg';
+}
+
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
 <html lang="<?php echo $func->getLanguageCode(); ?>">
@@ -19,12 +28,25 @@ $func->addStyle('/assets/css/style.min.css');
                     <img src="<?php echo $func->getUrl(); ?>/assets/img/monster2408-blog-top.jpg" alt="">
                 </div>
                 <div class="inner">
-                    <div class="recommend-title"><h2>おすすめ記事</h2></div>
-                    <div class="recommend-area" id="recommend-area"></div>
+                    <?php
+                        if (is_page() === false) {
+                            echo '<div class="recommend-title"><h2>おすすめ記事</h2></div>';
+                            echo '<div class="recommend-area" id="recommend-area"></div>';
+                        } else {
+                            echo '<div class="recommend-title"><h2 id="post-title"></h2></div>';
+                            echo '<div class="content" id="content-area"></div>';
+                        }
+                    ?>
                 </div>
             </div>
         </div>
         <?php include('./assets/include/footer.php'); ?>
-        <script src="<?php echo $func->getNoCacheUrl("/assets/js/feed-load.js"); ?>"></script>
+        <?php
+            if (is_page() === false) {
+                echo '<script src="'.$func->getNoCacheUrl("/assets/js/feed-load.js").'"></script>';
+            } else {
+                echo '<script src="'.$func->getNoCacheUrl("/assets/js/post-load.js").'"></script>';
+            }
+        ?>
     </body>
 </html>
